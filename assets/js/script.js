f460a5098e718ddbae91db35d285dd482333ca26
Ravnish Gupta <ravnish@gmail.com>
2022-02-21 19:22:02 -0800

    refactor code and using both label and dropdown values to create tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 var formEl = document.querySelector("#task-form");
-var ulTaskEl = document.querySelector("#to-do-tasks");
+//var ulTaskEl = document.querySelector("#to-do-tasks");
+var tasksToDoEl = document.querySelector("#tasks-to-do")
 formEl.addEventListener("submit", addTask);
 
 function addTask() {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
-    var liTaskEl = document.createElement("li");
-    liTaskEl.textContent = "New Task";
-    liTaskEl.className = "task-item";
-    ulTaskEl.appendChild(liTaskEl);
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 }
